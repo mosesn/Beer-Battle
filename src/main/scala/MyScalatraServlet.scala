9a6c2158157ea4673119301dc35bf22a72943b54
Augusto Corvalan <augusto.corvalan@gmail.com>
2012-01-28 19:22:21 -0500

    made sign-up template, added rudimentary get and post for sign-up in views

--- src/main/scala/MyScalatraServlet.scala
+++ src/main/scala/MyScalatraServlet.scala
@@ -25,7 +25,11 @@ class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
     println(request("password"))
   }
 
-
+  get("/signup") {
+    contentType = "text/html"
+    templateEngine.layout("/WEB-INF/layouts/signup.scaml")
+  }
+  
   notFound {
     // Try to render a ScalateTemplate if no route matched
     findTemplate(requestPath) map { path =>
