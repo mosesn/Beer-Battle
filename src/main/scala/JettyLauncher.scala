57967b174c095648766b29f5b831d07bf9ab08b1
Moses <nnythm@gmail.com>
2012-02-04 01:07:10 -0500

    is this working???

---
+++ src/main/scala/JettyLauncher.scala
@@ -0,0 +1,22 @@
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.webapp.WebAppContext
+
+object JettyLauncher {
+  def main(args: Array[String]) {
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 80
+
+    val server = new Server(port)
+    val context = new WebAppContext()
+    context setContextPath "/"
+    context.setResourceBase("src/main/webapp")
+    context.addServlet(classOf[MyScalatraServlet], "/*")
+    context.addServlet(classOf[DefaultServlet], "/")
+
+    server.setHandler(context)
+
+    server.start
+    server.join
+  }
+
+}
